897420f3c9a57dbc85305cc20542e536f83663b4
piosso1 <patrick2.udo@gmail.com>
2023-12-05 02:45:46 +0100

    correction

--- src/ProjectTask10.java
+++ src/ProjectTask10.java
@@ -1,6 +1,6 @@
 public class ProjectTask10 {
 
-    //Write a program to print out duplicate elements from an Array of Strings?
+    //Write a program to print out duplicate elements from an Array of Strings
     public static void main(String[] args) {
 
         String [] arrNames = {"ada","precious","kate","zeenab","kate","marry","kate"};
